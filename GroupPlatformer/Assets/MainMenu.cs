dc6ed5a9f18991d7b8b5207679cae15c2d2af6f5
Blamkauy <27.leonardo.ehm@ksd.org>
2025-10-16 09:42:19 -0700

    Imported TMPro; Created placeholder main menu

---
+++ GroupPlatformer/Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene(int sceneId)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneId);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
